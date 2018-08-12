6343cde621493e9e23ad081a10760e5e7d130093
Bo Lopker <blopker@23andme.com>
2018-08-11 22:22:51 -1000

    sourcemaps

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import MyComponent from '../../lib';
 import Link from '../../lib/Link';
 import './styles.css';
 
@@ -8,7 +7,6 @@ function Demo() {
   return (
     <div>
       <h1>Demo with examples of the component, hello </h1>
-      <MyComponent color='brown'>Wow what a button</MyComponent>
       <br/>
       <Link page='http://www.facebook.com'>Evil</Link>
     </div>
--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,24 +1,5 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import Link from './Link';
 
-class MySuperCoolComponent extends Component {
-  handleClick = () => {
-    // console.log('Click!');
-  };
-
-  render() {
-    const { color, children } = this.props;
-    return (
-      <button onClick={this.handleClick} style={{ color }}>
-        {children}
-      </button>
-    );
-  }
-}
-
-MySuperCoolComponent.propTypes = {
-  color: PropTypes.string,
-  children: PropTypes.element.isRequired,
+window.TTAM_UI = {
+  Link
 };
-
-export default MySuperCoolComponent;
